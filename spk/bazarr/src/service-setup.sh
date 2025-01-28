8cdbd945080d004d2102e836d95fb53778ad2bfb
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2025-01-28 09:47:48 -0400

    Make hybrid py311/py312 package

--- spk/bazarr/src/service-setup.sh
+++ spk/bazarr/src/service-setup.sh
@@ -1,5 +1,8 @@
 FFMPEG_DIR="/var/packages/ffmpeg7/target/bin"
 PYTHON_DIR="/var/packages/python312/target/bin"
+if [ "$SYNOPKG_DSM_VERSION_MAJOR" -lt 7 ] || [ "$SYNOPKG_DSM_ARCH" = "comcerto2k" ];then
+    PYTHON_DIR="/var/packages/python311/target/bin"
+fi
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${FFMPEG_DIR}:${PATH}"
 PYTHON="${SYNOPKG_PKGDEST}/env/bin/python3"
 LANGUAGE="env LANG=en_US.UTF-8 LC_ALL=en_US.utf8"
