7b82d5cf3747ec6680caa6632582f9dd566d9862
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2026-02-26 04:12:29 +0000

    postgresql: Modernize package structure
    - Unify service-setup.sh for DSM 6 and 7 using run_as_user() helper
    - Remove redundant conf.root directory and service-setup.as-root.sh
    - Consolidate wizards into single directory for both DSM versions
    - Remove wizard.dsm7 directory (redundant)
    - Improve wizard security: remove default password, add validation
    - Hardcode port 5433 in config (matches SERVICE_PORT, avoids conflicts)
    - Disable autovacuum on DSM 7 (non-root user lacks system table permissions)
    - Simplify upgrade wizard to informational only
    - Add connection instructions to install wizard
    - Use SERVICE_PORT variable instead of hardcoded port

--- spk/postgresql/src/service-setup.sh
+++ spk/postgresql/src/service-setup.sh
@@ -1,86 +1,100 @@
+# PostgreSQL service setup
+# Supports DSM 6 (runs as root) and DSM 7 (runs as package user)
 
-# package user is defined in conf/privilege and must not contain '-'.
+# Package user is defined in conf/privilege and must not contain '-'
 EFF_USER=sc_postgres
 
 DATABASE_DIR="${SYNOPKG_PKGVAR}/data"
 CFG_FILE="${DATABASE_DIR}/postgresql.conf"
 PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
 
-PG_USERNAME=${wizard_pg_username:=pgadmin}
-PG_PASSWORD=${wizard_pg_password:=changepassword}
-PG_PORT=${wizard_pg_port:=5433}
+# Wizard variables (required - no defaults)
+PG_USERNAME=${wizard_pg_username}
+PG_PASSWORD=${wizard_pg_password}
 
+# Backup variables from uninstall wizard
 PG_BACKUP=${wizard_pg_dump_directory}
-PG_BACKUP_CONF_DIR="${PG_BACKUP}/conf_`date +\"%d_%b\"`"
-PG_BACKUP_DUMP_DIR="${PG_BACKUP}/databases_`date +\"%d_%b\"`"
+PG_BACKUP_CONF_DIR="${PG_BACKUP}/conf_$(date +\"%d_%b\")"
+PG_BACKUP_DUMP_DIR="${PG_BACKUP}/databases_$(date +\"%d_%b\")"
 
-service_postinst ()
+# Helper function to run commands as the effective user
+# On DSM 7: scripts run as package user, so run directly
+# On DSM 6: scripts run as root, so use su to switch user
+run_as_user()
 {
-  # Init database and create default config file with UTF8 default encoding
-  ${SYNOPKG_PKGDEST}/bin/initdb -D ${DATABASE_DIR} --encoding=UTF8 --locale=en_US.UTF8
-
-  # update default config file
-  # - Change port according to the wizard
-  sed -e "s/^#port = 5432/port=${PG_PORT}/g" -i ${CFG_FILE}
-  # Change listen addresses
-  sed -e "s/^#listen_addresses = 'localhost'/listen_addresses = '*'/g" -i ${CFG_FILE}
-  # Disable autovacuum - running as non-root user lacks permissions on system tables
-  # Users can run VACUUM manually or set up a DSM scheduled task with root privileges
-  sed -e "s/^#autovacuum = on/autovacuum = off/g" -i ${CFG_FILE}
-
-  # Start server
-  ${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} -l ${LOG_FILE} start
-
-  # Update existing role
-  ${SYNOPKG_PKGDEST}/bin/psql -p ${PG_PORT} -d postgres -c "ALTER ROLE ${EFF_USER} WITH PASSWORD '${PG_PASSWORD}';"
-
-  # Create new role
-  ${SYNOPKG_PKGDEST}/bin/psql -p ${PG_PORT} -d postgres -c "CREATE ROLE ${PG_USERNAME} PASSWORD '${PG_PASSWORD}' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN REPLICATION BYPASSRLS;"
-
-  # Stop server
-  ${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} stop
-
-
-  echo "SAVE_PG_USERNAME=${wizard_pg_username}" > ${INST_VARIABLES}
-  echo "SAVE_PG_PASSWORD=${wizard_pg_password}" >> ${INST_VARIABLES}
-  echo "SAVE_PG_PORT=${wizard_pg_port}" >> ${INST_VARIABLES}
+    if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -ge 7 ]; then
+        eval "$@"
+    else
+        su - ${EFF_USER} -s /bin/sh -c "$@"
+    fi
 }
 
-
-service_preuninst ()
+service_postinst()
 {
-  # Backup on user's request
-  if [ "$wizard_pg_dump_database" = "true" ]; then
-
-    # Start server again
-    ${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} -l ${LOG_FILE} start
-
-    # Create backup conf dir
-    if [ ! -d ${PG_BACKUP_CONF_DIR} ]; then
-        mkdir -p ${PG_BACKUP_CONF_DIR}
-        chmod 755 ${PG_BACKUP_CONF_DIR}
+    # On DSM 6 (running as root), create data directory and set ownership
+    if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
+        mkdir -p "${DATABASE_DIR}"
+        chown -R ${EFF_USER}:sc-postgresql "${SYNOPKG_PKGVAR}"
     fi
 
-    # Backup config files
-    cp ${DATABASE_DIR}/pg_hba.conf ${PG_BACKUP_CONF_DIR}/
-    cp ${DATABASE_DIR}/pg_ident.conf ${PG_BACKUP_CONF_DIR}/
-    cp ${DATABASE_DIR}/postgresql.auto.conf ${PG_BACKUP_CONF_DIR}/
-    cp ${DATABASE_DIR}/postgresql.conf ${PG_BACKUP_CONF_DIR}/
+    # Initialize database cluster with UTF8 encoding
+    run_as_user "${SYNOPKG_PKGDEST}/bin/initdb -D ${DATABASE_DIR} --encoding=UTF8 --locale=en_US.UTF8"
 
-    # Create backup dump dir
-    if [ ! -d ${PG_BACKUP_DUMP_DIR} ]; then
-        mkdir -p ${PG_BACKUP_DUMP_DIR}
-        chmod 777 ${PG_BACKUP_DUMP_DIR}
+    # Configure postgresql.conf
+    # Set port from SERVICE_PORT (avoids conflict with Synology's PostgreSQL on 5432)
+    run_as_user "sed -e 's/^#port = 5432/port = ${SERVICE_PORT}/g' -i ${CFG_FILE}"
+    # Listen on all interfaces
+    run_as_user "sed -e \"s/^#listen_addresses = 'localhost'/listen_addresses = '*'/g\" -i ${CFG_FILE}"
+
+    # Disable autovacuum on DSM 7 - running as non-root user lacks permissions on system tables
+    # Users can run VACUUM manually or set up a DSM scheduled task with root privileges
+    if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -ge 7 ]; then
+        run_as_user "sed -e 's/^#autovacuum = on/autovacuum = off/g' -i ${CFG_FILE}"
     fi
 
-    # Backup all databases before uninstalling
-    for database in $(${SYNOPKG_PKGDEST}/bin/psql -A -t -p ${PG_PORT} -U ${PG_USERNAME} -d postgres -c "select datname from pg_database");
-    do
-        # Dump each database
-        ${SYNOPKG_PKGDEST}/bin/pg_dump -p ${PG_PORT} -U ${PG_USERNAME} -Fc ${database} >${PG_BACKUP_DUMP_DIR}/${database}.dump
-    done
+    # Start server temporarily to create roles
+    run_as_user "${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} -l ${LOG_FILE} start"
 
-    # Stop server
-    ${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} stop
-  fi
+    # Set password for the system user
+    run_as_user "${SYNOPKG_PKGDEST}/bin/psql -p ${SERVICE_PORT} -d postgres -c \"ALTER ROLE \\\"${EFF_USER}\\\" WITH PASSWORD '${PG_PASSWORD}';\""
+
+    # Create administrator role from wizard
+    run_as_user "${SYNOPKG_PKGDEST}/bin/psql -p ${SERVICE_PORT} -d postgres -c \"CREATE ROLE ${PG_USERNAME} PASSWORD '${PG_PASSWORD}' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN REPLICATION BYPASSRLS;\""
+
+    # Stop server (will be started by DSM)
+    run_as_user "${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} stop"
+}
+
+service_preuninst()
+{
+    # Backup databases on user's request
+    if [ "${wizard_pg_dump_database}" = "true" ]; then
+        # Start server for backup
+        run_as_user "${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} -l ${LOG_FILE} start"
+
+        # Create backup directories
+        if [ ! -d "${PG_BACKUP_CONF_DIR}" ]; then
+            mkdir -p "${PG_BACKUP_CONF_DIR}"
+            chmod 755 "${PG_BACKUP_CONF_DIR}"
+        fi
+
+        # Backup configuration files
+        cp "${DATABASE_DIR}/pg_hba.conf" "${PG_BACKUP_CONF_DIR}/"
+        cp "${DATABASE_DIR}/pg_ident.conf" "${PG_BACKUP_CONF_DIR}/"
+        cp "${DATABASE_DIR}/postgresql.auto.conf" "${PG_BACKUP_CONF_DIR}/"
+        cp "${DATABASE_DIR}/postgresql.conf" "${PG_BACKUP_CONF_DIR}/"
+
+        if [ ! -d "${PG_BACKUP_DUMP_DIR}" ]; then
+            mkdir -p "${PG_BACKUP_DUMP_DIR}"
+            chmod 755 "${PG_BACKUP_DUMP_DIR}"
+        fi
+
+        # Dump all databases (excluding templates)
+        for database in $(run_as_user "${SYNOPKG_PKGDEST}/bin/psql -A -t -p ${SERVICE_PORT} -d postgres -c \"SELECT datname FROM pg_database WHERE datistemplate = false\""); do
+            run_as_user "${SYNOPKG_PKGDEST}/bin/pg_dump -p ${SERVICE_PORT} -Fc ${database} > ${PG_BACKUP_DUMP_DIR}/${database}.dump"
+        done
+
+        # Stop server
+        run_as_user "${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} stop"
+    fi
 }
